1b9e719cbcf3a8845485abf06a5cfe2d72e50df7
Adam <2453760768@qq.com>
2022-06-21 10:10:29 +0800

    gitee同步测试

--- Test/Class1.cs
+++ Test/Class1.cs
@@ -11,13 +11,5 @@ namespace Test
         public int X;
         public int Y;
     }
-    class Program
-    {
-         struct Simple
-        {
-            public int x = 0;   // 编译错误
-            public int y = 10;   // 编译错误
-            public int prop1 { get; set; } = 5;    // 编译错误
-        }
-    }
+    // 推送到gitee测试
 }
