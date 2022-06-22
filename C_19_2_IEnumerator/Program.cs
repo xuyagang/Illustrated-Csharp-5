7056a44c2232bc16d262b424be9d919eec3abb81
Adam <2453760768@qq.com>
2022-06-22 11:01:35 +0800

    IEnumerable and IEnumerator

---
+++ C_19_2_IEnumerator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+
+namespace C_19_2_IEnumerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr1 = { 10, 11, 12, 13 };   // 创建数组
+            IEnumerator ie = arr1.GetEnumerator();   // 获取并存储枚举器
+            while (ie.MoveNext())
+            {
+                int item = (int)ie.Current;   // 获取当前项
+                Console.WriteLine($"Item value: {item}");
+            }
+            ie.Reset();
+        }
+    }
+}
