75ecacce8bf06fd26cd9e89f0cbea2fb09ad66c9
Adam <2453760768@qq.com>
2022-07-06 16:33:25 +0800

    泛型方法，扩展方法和泛型类

---
+++ C_18_5_泛型方法示例/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace C_18_5_泛型方法示例
+{
+    class Simple   // 非泛型类
+    {
+        static public void ReverseAndPrint<T>(T[] arr)   // 泛型方法
+        {
+            Array.Reverse(arr);
+            foreach (T item in arr)   // 使用参数类型T
+                Console.Write($"{item.ToString()}, ");
+            Console.WriteLine("");
+        }
+    }
+    public class Program
+    {
+        static void Main()
+        {
+            // 创建各种类型的数组
+            var intArray = new int[] { 3, 5, 7, 9, 11 };
+            var stringArray = new string[] { "first", "Second", "Third" };
+            var doubleArray = new double[] { 3.567, 7.891, 2.345 };
+
+            Simple.ReverseAndPrint<int>(intArray);   // 标准调用
+            Simple.ReverseAndPrint(intArray);        // 推断类型调用
+
+            Simple.ReverseAndPrint<string>(stringArray);
+            Simple.ReverseAndPrint(stringArray);
+
+            Simple.ReverseAndPrint<double>(doubleArray);
+            Simple.ReverseAndPrint(doubleArray);
+        }
+    }
+}
---
+++ C_18_6_扩展方法和泛型类/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+
+
+namespace C_18_6_扩展方法和泛型类
+{
+    // 定义一个Print扩展方法，扩展叫作Holder<T>的泛型类
+    static class ExtendHolder
+    {
+        // 声明泛型方法 + 扩展方法结合,方法名后需要加类型参数列表，形参类型前需要加this
+        public static void Print<T>(this Holder<T> h)
+        {
+            T[] vals = h.GetValues();
+            Console.WriteLine($"{vals[0]},\t{vals[1]},\t{vals[2]}");
+        }
+    }
+
+    class Holder<T>
+    {
+        T[] Vals = new T[3];
+        public Holder(T v0, T v1, T v2)
+        {Vals[0] = v0; Vals[1] = v1; Vals[2] = v2; }
+
+        // 返回数组
+        public T[] GetValues() { return Vals; }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var intHolder = new Holder<int>(3, 5, 7);
+            var stringHolder = new Holder<string>("A1", "A2", "A3");
+
+            intHolder.Print();
+            stringHolder.Print();
+        }
+    }
+}
