748256bf02350a78d9124e229edcf0c7221fec86
Adam <2453760768@qq.com>
2022-07-06 21:23:56 +0800

    枚举_flag

---
+++ C_12_2_1_Flags特性/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace C_12_2_1_Flags特性
+{
+    internal class Program
+    {
+        enum Suits { Spade = 1, Clubs = 2, Diamonds = 4, Hearts = 8 }
+        [Flags] enum SuitsFlags { Spade = 1, Clubs = 2, Diamonds = 4, Hearts = 8 }
+
+
+        [Flags] enum test { Spade=0, Clubs=9, Diamonds=5, Hearts=0}
+
+        static void Main(string[] args)
+        {
+            var str1 = (Suits.Spade | Suits.Diamonds).ToString();
+            var str2 = (SuitsFlags.Spade | SuitsFlags.Hearts).ToString();
+            var str3 = (test.Clubs | test.Hearts).ToString();
+
+            Console.WriteLine(str1);
+            Console.WriteLine(str2);
+            Console.WriteLine(str3);
+        }
+    }
+}
