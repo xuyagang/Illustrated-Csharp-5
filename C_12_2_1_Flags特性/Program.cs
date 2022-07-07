61fe91620fa23a55fe26f8411b46c76466fd8673
Adam <2453760768@qq.com>
2022-07-07 20:28:28 +0800

    无flags示例

--- C_12_2_1_Flags特性/Program.cs
+++ C_12_2_1_Flags特性/Program.cs
@@ -8,13 +8,13 @@ namespace C_12_2_1_Flags特性
         [Flags] enum SuitsFlags { Spade = 1, Clubs = 2, Diamonds = 4, Hearts = 8 }
 
 
-        [Flags] enum test { Spade=0, Clubs=9, Diamonds=5, Hearts=0}
+        [Flags] enum test { Spade=1, Clubs=19, Diamonds=8, Hearts=29}
 
         static void Main(string[] args)
         {
             var str1 = (Suits.Spade | Suits.Diamonds).ToString();
             var str2 = (SuitsFlags.Spade | SuitsFlags.Hearts).ToString();
-            var str3 = (test.Clubs | test.Hearts).ToString();
+            var str3 = (test.Diamonds | test.Spade).ToString();
 
             Console.WriteLine(str1);
             Console.WriteLine(str2);
---
+++ C_12_2_1_无flags示例/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace C_12_2_1_无flags示例
+{
+    // uint 无符合整型
+    [Flags]
+    enum CardDeckSettings : uint
+    {
+        SingleDeck = 0x01,
+        LargePicture = 0x02,
+        FancyNumbers = 0x04,
+        Animation = 0x08,
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CardDeckSettings ops;   // 创建标志字
+            ops = CardDeckSettings.FancyNumbers;   // 设置一个标志
+            Console.WriteLine(ops.ToString());
+
+            // 设置两个标志值
+            ops = CardDeckSettings.FancyNumbers | CardDeckSettings.Animation;
+            Console.WriteLine(ops.ToString());
+        }
+    }
+}
