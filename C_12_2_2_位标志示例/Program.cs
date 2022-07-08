f950adcdbeec4e572e0a21da22bbd241df56e6db
Adam <2453760768@qq.com>
2022-07-08 22:53:21 +0800

    完成枚举章节

--- C_12_2_2_位标志示例/Program.cs
+++ C_12_2_2_位标志示例/Program.cs
@@ -14,11 +14,13 @@ namespace C_12_2_2_位标志示例
 
     class MyClass
     {
+        // 声明多个字段
         bool UseSingleDeck               = false,
              UseBigPics                  = false,
              UseFancyNumber              = false,
              UseAnimation                = false,
              UseAnimationAndFancyNumbers = false;
+        // 通过传入的标志字判断是否含有位标志，对字段进行赋值
         public void SetOptions(CardDeckSettings ops)
         {
             UseSingleDeck = ops.HasFlag(CardDeckSettings.SingleDeck);
@@ -27,7 +29,16 @@ namespace C_12_2_2_位标志示例
             UseAnimation = ops.HasFlag(CardDeckSettings.Animation);
 
             CardDeckSettings testFlags = CardDeckSettings.Animation | CardDeckSettings.FancyNumbers;
-            UseAnimationAndFancyNumbers = ops.HasFlag(testFlags);
+            UseAnimationAndFancyNumbers = ops.HasFlag(testFlags);   // true
+        }
+        public void PrintOptions()
+        {
+            Console.WriteLine("Option Settings:");
+            Console.WriteLine("Use Single Deck - {0}", UseSingleDeck);
+            Console.WriteLine("Use Large Pictures - {0}", UseBigPics);
+            Console.WriteLine("Use Fancy Numbers - {0}", UseFancyNumber);
+            Console.WriteLine("Show Animation - {0}", UseAnimation);
+            Console.WriteLine("Show Animation and FancyNumbers - {0}", UseAnimationAndFancyNumbers);
         }
     }
 
@@ -35,7 +46,13 @@ namespace C_12_2_2_位标志示例
     {
         static void Main(string[] args)
         {
-            
+            // 创建标志字，并对位标志进行设置
+            CardDeckSettings ops = CardDeckSettings.SingleDeck
+                                 | CardDeckSettings.FancyNumbers
+                                 | CardDeckSettings.Animation;
+            MyClass mc = new MyClass();
+            mc.SetOptions(ops);
+            mc.PrintOptions();
         }
     }
 }
---
+++ C_12_3_枚举补充/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace C_12_3_枚举补充
+{
+    enum TrafficLight
+    {
+        Green,
+        Yellow,
+        Red,
+    }
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Second member of TrafficLight is {0}\n",
+                               Enum.GetName(typeof(TrafficLight), 1));
+
+            Console.WriteLine(Enum.GetName(typeof(TrafficLight), 5) == null);
+            Console.WriteLine();
+
+            foreach (var name in Enum.GetNames(typeof(TrafficLight)))
+                Console.WriteLine(name);
+        }
+    }
+}
