6241d40f57be2e08e8014b10cada4f44672b586c
Adam <2453760768@qq.com>
2022-08-18 20:43:04 +0800

    temp

--- C_18_10_1_协变/Program.cs
+++ C_18_10_1_协变/Program.cs
@@ -2,7 +2,6 @@
 
 namespace C_18_10_1_协变
 {
-<<<<<<< HEAD
     class Animal { public int Legs = 4; }   // 基类
     class Dog : Animal { }   // 派生类
 
@@ -24,43 +23,3 @@ namespace C_18_10_1_协变
         }
     }
 }
-=======
-    //class Animal
-    //{
-    //    public int NumberOfLegs = 4;
-    //}
-
-    //class Dog : Animal
-    //{ 
-    //}
-
-    //class Program
-    //{
-    //    static void Main(string[] args)
-    //    {
-    //        Animal a1 = new Animal();
-    //        Animal a2 = new Dog();
-
-    //        Console.WriteLine($"Number of dog legs: {a2.NumberOfLegs}");
-    //    }
-    //}
-
-
-
-    class Animal { public int Legs = 4;}   // 基类
-    class Dog : Animal { }   // 派生类
-    delegate T Factory<out T>();   // 委托
-    class Program
-    {
-        static Dog MakeDog()   // 符合Factory委托的方法
-        {
-            return new Dog();
-        }
-        static void Main()
-        {
-            Factory<Dog> dogMaker = MakeDog;   // 创建委托对象
-            Factory<Animal> animalMaker = dogMaker;   // 尝试赋值委托对象，会编译错误
-        }
-    }
-}
->>>>>>> 5f48c034837d27efc5c9bcc32a6c71cc46756570
