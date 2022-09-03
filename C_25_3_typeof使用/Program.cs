d2fa5ad2c7a794110ebec3f8a0042684c2f6ede0
Adam <2453760768@qq.com>
2022-09-04 00:10:48 +0800

    特性

---
+++ C_25_3_typeof使用/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Reflection;
+
+namespace C_25_3_typeof使用
+{
+    class BaseClass
+    {
+        public int BaseField;
+    }
+    class DerivedClass: BaseClass
+    {
+        public int DerivedField;
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Type tbc = typeof(DerivedClass);   // 获取类型
+            Console.WriteLine($"Object type: {tbc.Name}");
+            FieldInfo[] fi = tbc.GetFields();
+            foreach (var f in fi)
+            {
+                Console.WriteLine($"      Field: {f.Name}");
+            }
+
+        }
+    }
+}
---
+++ C_25_3_获取type对象/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Reflection;
+
+namespace C_25_3_获取type对象
+{
+    class BaseClass
+    {
+        public int _BaseField = 0;
+    }
+    class DerivedClass : BaseClass
+    {
+        public int _DerivedField = 0;
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var bc = new BaseClass();
+            var dc = new DerivedClass();
+
+            BaseClass[] bca = new BaseClass[] {bc, dc};
+
+            foreach (var v in bca)
+            {
+                Type t = v.GetType();
+                Console.WriteLine($"Object type: {t.Name}");
+
+                FieldInfo[] fi = t.GetFields();
+                foreach(var f in fi)
+                {
+                    Console.WriteLine($"      Field: {f.Name}");
+                }    
+                Console.WriteLine();
+            }
+        }
+    }
+}
