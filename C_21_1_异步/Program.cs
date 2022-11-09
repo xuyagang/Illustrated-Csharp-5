6161667920dedfea54a8f6282d9d488373207359
Adam <2453760768@qq.com>
2022-11-09 22:09:58 +0800

    非异步实现

---
+++ C_21_1_异步/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace C_21_1_异步
+{
+    class MyDownloadString
+    {
+        Stopwatch sw = new Stopwatch();
+        
+        public void DoRun()
+        {
+            const int LargeNumber = 6000000;
+            sw.Start();
+
+        }
+
+
+
+
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- C_21_1_非异步/Program.cs
+++ C_21_1_非异步/Program.cs
@@ -9,23 +9,39 @@ namespace C_21_1_非异步
     class MyDownloadString
     {
         //初始化新的 Stopwatch 实例，将运行时间属性设置为零，然后开始测量运行时间
-        Stopwatch sw = Stopwatch.StartNew();
+        // Stopwatch sw1 = Stopwatch.StartNew();   // 创建并开始计时
+        Stopwatch sw = new Stopwatch();
 
         public void DoRun()
         {
             const int LargeNumber = 6000000;
-            sw.Stop();
+            sw.Start();
+            int t1 = CountCharacter(1, "https://cn.bing.com/");
+            int t2 = CountCharacter(2, "https://www.baidu.com/");
 
-            
+            CountToALargeNumber(1, LargeNumber); CountToALargeNumber(2, LargeNumber);
+            CountToALargeNumber(3, LargeNumber); CountToALargeNumber(4, LargeNumber);
+
+            Console.WriteLine($"Chars in https://cn.bing.com/      : {t1}");
+            Console.WriteLine($"Chars in https://www.baidu.com/    : {t2}");
+            sw.Stop();
         }
 
         private int CountCharacter(int id, String uriString)
         {
             WebClient wc1 = new WebClient();
-            Console.WriteLine($"Start call {id}     :     {sw.Elapsed.TotalMilliseconds,4:No}");
+            // 格式化数字字符串 {索引, 宽度对齐:格式精度}
+            Console.WriteLine($"Start call {id}       :     {sw.Elapsed.TotalMilliseconds, 4:N0}");
             string result = wc1.DownloadString(new Uri(uriString));
-            Console.WriteLine($" Call {id} completed:     {sw.Elapsed.TotalMilliseconds}");
+            Console.WriteLine($"Call {id} completed   :     {sw.Elapsed.TotalMilliseconds, 4:N0}");
             return result.Length;
+        }
+
+        private void CountToALargeNumber(int id, int value)
+        {
+            for (long i = 0; i < value; i++)
+                ;
+            Console.WriteLine($"End counting {id}     :     {sw.Elapsed.TotalMilliseconds,4:N0}");
         }
     }
 
