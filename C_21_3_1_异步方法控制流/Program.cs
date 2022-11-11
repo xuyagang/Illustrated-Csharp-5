7c0c1aee0ccaa7f02334eadf2ab8418adbc20edc
Adam <2453760768@qq.com>
2022-11-11 15:50:51 +0800

    空条件运算符

---
+++ C_21_3_1_异步方法控制流/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace C_21_3_1_异步方法控制流
+{
+    internal class Program
+    {
+        public int Get10()   // 与Func<int> 兼容
+        {
+            return 10;
+        }
+
+        public async Task DoWorkAsync()
+        {
+            Func<int> ten = new Func<int>(Get10);
+            int a = await Task.Run(ten);
+            int b = await Task.Run(new Func<int>(Get10));
+            int c = await Task.Run(() => { return 10; });
+            Console.WriteLine($"{a} {b} {c}");
+        }
+
+        static void Main(string[] args)
+        {
+            Task t = (new Program()).DoWorkAsync();
+            t.Wait();
+        }
+    }
+}
---
+++ C_26_7_空条件运算符/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace C_26_7_空条件运算符
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student[] students = null;
+
+            //int studentCount = students.Length;
+            //Student first = students[0];
+
+            int? studentCount = students?.Length;
+            Student first = students?[0];
+
+        }
+    }
+
+    class Student
+    {
+
+    }
+}
