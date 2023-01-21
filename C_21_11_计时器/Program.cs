5b581ef7aff510fc6387f827d650e55e33dcf617
Adam <2453760768@qq.com>
2023-01-21 23:44:47 +0800

    计时器

---
+++ C_21_11_计时器/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace C_21_11_计时器
+{
+    class Program
+    {
+        int TimesCalled = 0;
+
+        void Display(object state)
+        {
+            Console.WriteLine($"{(string)state} {++TimesCalled}");
+        }
+
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            //                                                   2秒后第一次调用  每秒重复一次
+            Timer myTimer = new Timer(p.Display, "Processing timer event", 2000, 1000);
+            Console.WriteLine("Timer started");
+            Console.ReadLine();
+        }
+    }
+}
