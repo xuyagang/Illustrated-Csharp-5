f6ad68c3ce11c6920d342958806f94acbeb81433
Adam <2453760768@qq.com>
2023-01-22 23:02:59 +0800

    完成计时器

--- C_21_11_计时器/Program.cs
+++ C_21_11_计时器/Program.cs
@@ -9,8 +9,9 @@ namespace C_21_11_计时器
 {
     class Program
     {
+        // 调用次数
         int TimesCalled = 0;
-
+        // 回调函数，打印调用次数信息
         void Display(object state)
         {
             Console.WriteLine($"{(string)state} {++TimesCalled}");
@@ -19,6 +20,8 @@ namespace C_21_11_计时器
         static void Main(string[] args)
         {
             Program p = new Program();
+
+            // Timer提供以指定的时间间隔对线程池线程执行方法的机制
             //                                                   2秒后第一次调用  每秒重复一次
             Timer myTimer = new Timer(p.Display, "Processing timer event", 2000, 1000);
             Console.WriteLine("Timer started");
