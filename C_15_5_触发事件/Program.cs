776c714285f4e8142140d17e9301030d3187707f
Adam <2453760768@qq.com>
2023-01-23 23:00:24 +0800

    开始事件学习

---
+++ C_15_5_触发事件/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_15_5_触发事件
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
--- C_21_11_计时器/Program.cs
+++ C_21_11_计时器/Program.cs
@@ -22,7 +22,7 @@ namespace C_21_11_计时器
             Program p = new Program();
 
             // Timer提供以指定的时间间隔对线程池线程执行方法的机制
-            //                                                   2秒后第一次调用  每秒重复一次
+            //                                                   2秒后第一次调用   每秒重复一次
             Timer myTimer = new Timer(p.Display, "Processing timer event", 2000, 1000);
             Console.WriteLine("Timer started");
             Console.ReadLine();
