3828b1dcfb0b4f5e769fca7c841d032cc9f36e84
Adam <2453760768@qq.com>
2023-01-25 21:48:38 +0800

    标准事件用法

--- C_15_5_触发事件/Program.cs
+++ C_15_5_触发事件/Program.cs
@@ -10,20 +10,26 @@ namespace C_15_5_触发事件
 
     /// <summary>
     /// 发布者
+    /// 定义事件，并在自身方法中触发事件（调用自己的委托）
     /// </summary>
     class Incrementer
     {
         public event Handler CountedADozen;   // 创建事件并发布（委托类型）
+        /// <summary>
+        /// 发布者方法
+        /// </summary>
         public void DoCount()
         {
             for (int i = 0; i < 100; i++)
                 if (i % 12 == 0 && CountedADozen != null)
-                    CountedADozen();   // 每增加12个计数触发事件一次
+                    CountedADozen();   // 每增加12个计数，触发事件一次
         }
     }
 
     /// <summary>
     /// 订阅者
+    /// 定义事件处理程序（可以在外部类中）
+    /// 注册事件，向传入的发布者类的委托添加事件函数
     /// </summary>
     class Dozens
     {
@@ -31,7 +37,8 @@ namespace C_15_5_触发事件
         public Dozens(Incrementer incrementer)
         {
             DozensCount = 0;
-            incrementer.CountedADozen += IncrementDozensCount;   // 订阅事件
+            // 注册在事件成员被触发时要调用的回调方法
+            incrementer.CountedADozen += IncrementDozensCount;   
         }
 
         // 声明事件处理程序
@@ -46,9 +53,10 @@ namespace C_15_5_触发事件
         static void Main(string[] args)
         {
             Incrementer incrementer = new Incrementer();      // 实例化发布者
-            Dozens dozensCounter = new Dozens(incrementer);   // 使用发布者实例为参数实例化订阅者 
-
-            incrementer.DoCount();   // 在发布者的方法中会触发事件，会调用委托，委托会在初始化订阅者时配置
+            // 使用发布者实例为参数实例化订阅者，并为发布者类的委托添加事件函数
+            Dozens dozensCounter = new Dozens(incrementer);   
+            // 在发布者的方法中会触发事件，会调用委托，委托会在初始化订阅者时配置
+            incrementer.DoCount();   
             Console.WriteLine("Number of dozens = {0}", dozensCounter.DozensCount);
         }
     }
---
+++ C_15_6_标准事件的用法/Program.cs
@@ -0,0 +1,61 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_15_6_标准事件的用法
+{
+    /// <summary>
+    /// 发布者
+    /// </summary>
+    class Incrementer
+    {
+        // 使用系统定义的EventHandler委托，创建事件并发布
+        public event EventHandler CountedADozen;
+        // 发布者方法，会在其中触发事件
+        public void DoCount()
+        {
+            for (int i = 0; i < 100; i++)
+                if (i % 12 == 0 && CountedADozen != null)
+                    CountedADozen(this, null);   // 触发事件时使用EventHandler的参数
+        }
+    }
+    /// <summary>
+    /// 订阅者
+    /// </summary>
+    class Dozens
+    {
+        public int DozensCount { get; private set; }
+        /// <summary>
+        /// 订阅者的构造函数，参数是发布者实例，通过其委托添加事件处理程序
+        /// </summary>
+        public Dozens(Incrementer incrementer)
+        {
+            DozensCount = 0;
+            incrementer.CountedADozen += IncrementDozensCount;
+        }
+        /// <summary>
+        /// 事件处理程序的签名必须与委托的签名匹配
+        /// </summary>
+        void IncrementDozensCount(object source, EventArgs e)
+        {
+            DozensCount++;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 实例化发布者
+            Incrementer incrementer = new Incrementer();
+            // 使用发布者实例为参数实例化订阅者，其构造函数会为发布者类的委托添加事件函数
+            Dozens dozensCounter = new Dozens(incrementer);
+            // 调用发布者的方法,其在特定条件会触发事件,会执行委托，然后其会执行它所持有的方法
+            incrementer.DoCount();
+            Console.WriteLine("Number of dozens = {0}", dozensCounter.DozensCount);
+        }
+    }
+}
