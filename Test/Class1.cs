c9c6c90d292efe1555ba13957bc968b91dfacb60
Adam <2453760768@qq.com>
2023-01-26 22:18:47 +0800

    语法新增特性学习ing

--- Test/Class1.cs
+++ Test/Class1.cs
@@ -6,10 +6,16 @@ using System.Threading.Tasks;
 
 namespace Test
 {
-    struct Simple
+    class Program
     {
-        public int X;
-        public int Y;
+        //private double myConstant = 56.7;
+        //public double Length4 { get; set; } = myConstant;
+
+        private double myProperty { get; set; } = 67.8;
+        public double Length5 { get; set; } = myProperty;
+        static void Main()
+        {
+            Console.WriteLine(new  Program().Length4);
+        }
     }
-    // 推送到gitee测试2
 }
