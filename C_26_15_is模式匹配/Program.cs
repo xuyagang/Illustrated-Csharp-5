d114dae97005aea38431a6a2f0f2568b3d770429
Adam <2453760768@qq.com>
2023-01-30 23:52:06 +0800

    is模式匹配

---
+++ C_26_15_is模式匹配/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_26_15_is模式匹配
+{
+    public interface IOne
+    {
+        int SampleIntProperty { get; set; }
+    }
+    public interface ITwo
+    {
+        int SampleIntProperty2 { get; set; }
+    }
+
+    public class BaseClass
+    {
+        public string SampleStringProperty { get; set; }
+    }
+    public class DerivedClass : BaseClass, IOne, ITwo
+    {
+        public int SampleIntProperty { get; set; }
+        public int SampleIntProperty2 { get; set; }  
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dc = new DerivedClass();
+            if (dc is DerivedClass)
+            { Console.WriteLine("Derived Class found"); }
+
+            if (dc is BaseClass)
+            { Console.WriteLine("Base Class found"); }
+
+            if (dc is IOne)
+            { Console.WriteLine("Interface One found"); }
+
+            if (dc is ITwo)
+            { Console.WriteLine("Interface Tow found"); }
+        }
+    }
+}
--- Test/Class1.cs
+++ Test/Class1.cs
@@ -8,14 +8,6 @@ namespace Test
 {
     class Program
     {
-        //private double myConstant = 56.7;
-        //public double Length4 { get; set; } = myConstant;
 
-        private double myProperty { get; set; } = 67.8;
-        public double Length5 { get; set; } = myProperty;
-        static void Main()
-        {
-            Console.WriteLine(new  Program().Length4);
-        }
     }
 }
