e0535622a66e492db3af53e4ce6d29f695c09a02
Adam <aaadam@mail.com>
2023-07-16 21:14:39 +0800

    XML创建

---
+++ C_20_7_3_XML创建/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace C_20_7_3_XML创建
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            XDocument employees1 =
+                new XDocument(                                  //xml文档
+                    new XElement("Employees",                   //根元素
+                        new XElement("Name", "Bob Smith"),      //元素
+                        new XElement("Name", "Sally Jones")     //元素
+                    )
+                );
+
+            employees1.Save("EmployeesFile.xml");   //保存到文件
+
+            // 加载文档到变量
+            XDocument employee2 = XDocument.Load("EmployeesFile.xml");
+            Console.WriteLine(employee2);
+
+            Console.ReadKey();
+
+        }
+    }
+}
