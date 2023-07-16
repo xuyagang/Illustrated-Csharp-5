f88f116f6270d051f95fe97c5bffde5dec702bb2
Adam <2453760768@qq.com>
2023-07-16 12:04:27 +0800

    使用Lambda表达式参数

---
+++ C_20_6_6_使用Lambda表达式参数/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_20_6_6_使用Lambda表达式参数
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Lambda
+            int[] intArray = { 1, 2, 3, 4, 5, 6 };
+            var countOdd = intArray.Count(x => x % 2 == 1);
+            Console.WriteLine(countOdd);
+            Console.ReadKey();
+            #endregion
+
+            #region 匿名方法
+            int[] intArray2 = { 1, 2, 3, 4, };
+            // 匿名方法
+            Func<int, bool> myDel = delegate(int x) { return x % 2 == 1; };
+            var countOddd = intArray2.Count(myDel);
+            Console.WriteLine(countOddd);
+            Console.ReadKey();
+            #endregion
+        }
+    }
+}
