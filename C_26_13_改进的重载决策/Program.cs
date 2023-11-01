3d4dedb7fab4a76bbf01bb62769aef91f1f91179
Adam <2453760768@qq.com>
2023-11-01 16:55:27 +0800

    改进的重载决策

---
+++ C_26_13_改进的重载决策/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_26_13_改进的重载决策
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 之前因为二义性会报错
+            // c#6.0 优先选择 Func<Task> function 而不是 Action
+            Task.Run(DoThings);
+
+            // Action
+            Task.Run(Test);
+        }
+        static Task DoThings()
+        {
+            return Task.FromResult(0);
+        }
+
+        static void Test() { }
+    }
+}
