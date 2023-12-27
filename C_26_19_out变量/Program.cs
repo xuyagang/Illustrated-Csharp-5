c64e6004c324f3a3ad617f4702618456690c783c
Adam <2453760768@qq.com>
2023-12-27 13:36:59 +0800

    temp

---
+++ C_26_19_out变量/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_26_19_out变量
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public int? OutParameterSample()
+        {
+            return null;
+        }
+    }
+}
