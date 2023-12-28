57cb66255a64afdd9ca81107b6a40c765b54e4be
Adam <2453760768@qq.com>
2023-12-28 20:38:07 +0800

    局部函数示例1

---
+++ C_26_20_局部函数/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_26_20_局部函数
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> data = GetDataFromDb();
+            foreach(var item in data)
+            {
+                Console.WriteLine(ReplaceEmptyStringWithElipsis(item));
+            }
+            string ReplaceEmptyStringWithElipsis(string input) 
+            {
+                if (string.IsNullOrEmpty(input)) return "...";
+                return input;
+            }
+        }
+        private static List<string> GetDataFromDb()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
