3ae68febf73ab5c2ef31c6db5e7e3b011d82e9a9
Adam <2453760768@qq.com>
2024-01-08 14:32:25 +0800

    tryparse

---
+++ C_27_4_Parse/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_27_4_Parse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "25.873";
+            string s2 = "36.240";
+
+            double d1 = double.Parse(s1);
+            double d2 = double.Parse(s2);
+
+            double total = d1 + d2;
+            Console.WriteLine($"Total:{total}");
+        }
+    }
+}
---
+++ C_27_4_TryParse/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_27_4_TryParse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
