165538670017d605ba99a393f270ee9fe459d523
Adam <2453760768@qq.com>
2024-01-08 14:02:20 +0800

    StringBuilder

---
+++ C_27_3_StringBuilder/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_27_3_StringBuilder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder("Hi there.");
+            Console.WriteLine(sb.ToString());
+            sb.Replace("Hi", "Hello");
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
