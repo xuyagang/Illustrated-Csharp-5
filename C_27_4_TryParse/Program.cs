518152857c985b310f1c90cc01f83860acdc0bcf
Adam <2453760768@qq.com>
2024-01-08 15:10:56 +0800

    tryParse

--- C_27_4_TryParse/Program.cs
+++ C_27_4_TryParse/Program.cs
@@ -10,6 +10,26 @@ namespace C_27_4_TryParse
     {
         static void Main(string[] args)
         {
+            string parseResultSummary;
+            string stringFirst = "28";
+            int intFirst;
+            //                          输入字符串    输出变量
+            bool success = int.TryParse(stringFirst, out intFirst);
+            parseResultSummary = success 
+                                ? "was successfully parsed"
+                                : "was not successfully parsed";
+            Console.WriteLine($"String {stringFirst} {parseResultSummary}");
+            Console.WriteLine($"{intFirst}");
+
+
+            string stringSecond = "vt750";
+            int intSecond;
+            success = int.TryParse(stringSecond, out intSecond);
+            parseResultSummary = success
+                                ? "was successfully parsed"
+                                : "was not successfully parsed";
+            Console.WriteLine($"String {stringSecond} {parseResultSummary}");
+            Console.WriteLine($"{intSecond}");
         }
     }
 }
