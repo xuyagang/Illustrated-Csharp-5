7bf11653978cb1bb1e10ce038df64381b0d034f2
Adam <2453760768@qq.com>
2024-01-08 13:45:22 +0800

    字符串

---
+++ C_27_2_字符串/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_27_2_字符串
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "hi there! this, is: a string.";
+            char[] delimiters = { ' ', '!', ',', '.' };
+            string[] words = s1.Split(delimiters);
+            foreach(string word in words)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
