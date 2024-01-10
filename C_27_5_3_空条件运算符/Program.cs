8d36ced9120a348a3073653fdbdc240126b3eb9d
Adam <2453760768@qq.com>
2024-01-10 15:20:16 +0800

    空条件运算符

---
+++ C_27_5_3_空条件运算符/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_27_5_3_空条件运算符
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student[] students = null;
+            int? studentCount = 5;
+
+            if (students != null)   // 访问前进行检查
+                studentCount = students.Length;   // 访问非空引用
+            Console.WriteLine($"StudentCount:{studentCount}");
+
+            // 对于引用变量，使用空条件运算符 ?. 方法其属性
+            studentCount = students?.Length;
+            Console.WriteLine($"StudentCount:{studentCount}");
+
+
+
+            //int length = 3;
+            //int strLength = length?.ToString();
+
+
+
+
+        }
+    }
+
+    class Student { }
+}
