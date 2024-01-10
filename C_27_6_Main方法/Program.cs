6a398d4f9a22b32063072e4be25061bde69d373a
Adam <2453760768@qq.com>
2024-01-10 19:50:23 +0800

    main

---
+++ C_27_6_Main方法/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_27_6_Main方法
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (string arg in args)
+            {
+                Console.WriteLine(arg);
+            }
+        }
+    }
+}
