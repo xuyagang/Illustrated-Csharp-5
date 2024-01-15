8b2fa94c7a9f90b600eac9853a0a34f5b7fd097b
Adam <2453760768@qq.com>
2024-01-15 14:02:18 +0800

    嵌套类型

---
+++ C_27_8_嵌套类型/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_27_8_嵌套类型
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass mc = new MyClass();
+            mc.Incr(); mc.Incr(); mc.Incr();
+            mc.Incr(); mc.Incr(); mc.Incr();
+            Console.WriteLine($"Total:{mc.GetValue()}");
+        }
+    }
+
+    class MyClass  // 封闭类
+    {
+        class MyCounter  // 嵌套类
+        {
+            public int Count { get; set; }
+            public static MyCounter operator ++(MyCounter current)
+            {
+                current.Count++;
+                return current;
+            }
+        }
+
+        // 嵌套类类型的字段
+        private MyCounter counter;
+        // 构造函数
+        public MyClass() { counter = new MyCounter(); }
+        // 增量方法
+        public int Incr() { return (counter++).Count; }
+        // 获取计数
+        public int GetValue() { return counter.Count; }
+    }
+}
