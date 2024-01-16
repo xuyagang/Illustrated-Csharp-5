7c5eec589ffe149bdb6b21afc2dc50f65bde2405
Adam <2453760768@qq.com>
2024-01-16 10:58:46 +0800

    tuple

---
+++ C_27_10_元组/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_27_10_元组
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var primes = Tuple.Create(2, 3, 5, 7, 11, 13, 17, 19);
+            Console.WriteLine("Prime numbers less than 20: " +
+                              "{0}, {1}, {2}, {3}, {4}, {5}, {6}, and {7}, {8}",
+                              primes.Item1, primes.Item2, primes.Item3,
+                              primes.Item4, primes.Item5, primes.Item6,
+                              primes.Item7, primes.Rest.Item1, primes.Rest.Item2);
+        }
+    }
+}
